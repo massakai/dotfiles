8345420a749dbcbfae3efcebc7984830ea2b53cf
Masashi SAKAI <masashi.sakai1986@gmail.com>
2014-08-13 20:41:23 +0900

    remove command prefix

--- setup.sh
+++ setup.sh
@@ -16,21 +16,14 @@ echo $0
 echo ${REPOSITORY_ROOT_DIR}
 for dotfile in $(find ${REPOSITORY_ROOT_DIR} -name ".*" -type f | grep -v '\.git$')
 do
-  case "${OSTYPE}" in
-  darwin*)
-    FILENAME=$(/usr/bin/basename ${dotfile})
-    ;;
-  linux*)
-    FILENAME=$(/bin/basename ${dotfile})
-    ;;
-  esac
-  /bin/rm -f ~/${FILENAME}
-  /bin/ln -s ${dotfile} ~/${FILENAME}
+  FILENAME=$(basename ${dotfile})
+  rm -f ~/${FILENAME}
+  ln -s ${dotfile} ~/${FILENAME}
 done
 
 mkdir -p ~/.ssh
-/bin/rm -f ~/.ssh/config
-/bin/ln -s ${REPOSITORY_ROOT_DIR}/.ssh/config ~/.ssh/config
+rm -f ~/.ssh/config
+ln -s ${REPOSITORY_ROOT_DIR}/.ssh/config ~/.ssh/config
 
 # cron
 crontab ${REPOSITORY_ROOT_DIR}/crontab
