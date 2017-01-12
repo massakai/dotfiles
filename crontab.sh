5f47ee61adc3224f52836ec0f77e9e41a49aa046
Masashi SAKAI <masashi.sakai1986@gmail.com>
2017-01-12 22:29:58 +0900

    crontabをシェルスクリプトに変更

--- crontab.sh
+++ crontab.sh
@@ -1,7 +1,15 @@
-MAILTO=masashi.sakai1986@gmail.com
-PATH=~/.crontab/bin;/usr/local/bin;/usr/bin;/bin
-LOG_DIR=~/.crontab/logs
+#!/bin/sh
 
+cat <<EOS
+SHELL=${SHELL}
+HOME=${HOME}
+MAILTO=masashi.sakai1986@gmail.com
+PATH=${HOME}/.crontab/bin;/usr/local/bin;/usr/bin;/bin
+LOG_DIR=${HOME}/.crontab/logs
+
+EOS
+
+cat <<'EOS'
 #
 # hourly
 #
@@ -18,3 +26,4 @@ LOG_DIR=~/.crontab/logs
 
 # remove old log files
 0 0 * * * find ${LOG_DIR} -name '*.out' -o -name '*.err' -mtime +30 | xargs ${XARGS_EMPTY_OPTION} rm
+EOS
