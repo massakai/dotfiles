cfd95a1b5d584847da7e439e9729a52f9974b0a4
Masashi SAKAI <masashi.sakai1986@gmail.com>
2017-10-26 07:33:56 +0900

    実行時に前提となるディレクトリ、ファイルが存在しない場合は、ログを出してexitするように修正

--- .crontab/bin/brew.sh
+++ .crontab/bin/brew.sh
@@ -5,6 +5,12 @@ LOG_DIR=~/.crontab/logs/brew
 BREW_LOG=${LOG_DIR}/$DATE.out
 BREW_ERR=${LOG_DIR}/$DATE.err
 
+if [[ $(which brew) != 0 ]]
+then
+    echo "Homebrewがインストールされていません。" > ${BREW_ERR}
+    exit 1
+fi
+
 mkdir -p ${LOG_DIR}
 brew update >> ${BREW_LOG} 2>> ${BREW_ERR}
 brew upgrade >> ${BREW_LOG} 2>> ${BREW_ERR}
--- .crontab/bin/oxr_getter.sh
+++ .crontab/bin/oxr_getter.sh
@@ -1,10 +1,28 @@
 #!/bin/bash -eu
 
+DATE=`date "+%Y-%m-%d %H:%M:%S"`
+
 # settings
 APP_ENV=~/.venvs/pyoxr
 APP_HOME=~/github/pyoxr
-APP_ID=`cat ~/Dropbox/Backup/oxr.app_id`
+APP_ID_PATH=~/Dropbox/Backup/oxr.app_id
 LOG=~/Dropbox/Backup/oxr.out
 
+if [[  ! -d ${APP_ENV} ]]
+then
+    echo "${APP_ENV}が存在しません。" >> ${LOG}
+    exit 1
+elif [[ ! -d ${APP_HOME} ]]
+then
+    echo "${APP_HOME}が存在しません。" >> ${LOG}
+    exit 1
+elif [[ ! -d ${APP_ID_PATH} ]]
+then
+    echo "${APP_ID_PATH}が存在しません。" >> ${LOG}
+    exit 1
+fi
+
+APP_ID=`cat ${APP_ID_PATH}`
 source $APP_ENV/bin/activate
+echo "[${DATE}] start oxr_getter.py" >> ${LOG}
 python3.5 $APP_HOME/oxr_getter.py >> $LOG
