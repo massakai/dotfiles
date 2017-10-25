940003220edbfc31f433dbd05ad6b9c04f73c95a
Masashi SAKAI <masashi.sakai1986@gmail.com>
2017-10-26 07:36:03 +0900

    Macbook Proにはまだインストールしていないので、メールが飛ばないように終了コードを0に変更

--- .crontab/bin/brew.sh
+++ .crontab/bin/brew.sh
@@ -8,7 +8,8 @@ BREW_ERR=${LOG_DIR}/$DATE.err
 if [[ $(which brew) != 0 ]]
 then
     echo "Homebrewがインストールされていません。" > ${BREW_ERR}
-    exit 1
+    # インストールしない環境もあるので、エラーにはしない。
+    exit 0
 fi
 
 mkdir -p ${LOG_DIR}
--- .crontab/bin/oxr_getter.sh
+++ .crontab/bin/oxr_getter.sh
@@ -8,18 +8,21 @@ APP_HOME=~/github/pyoxr
 APP_ID_PATH=~/Dropbox/Backup/oxr.app_id
 LOG=~/Dropbox/Backup/oxr.out
 
+# インストールしない環境もあるので、エラーにはしない。
+ERROR_EXIT_CODE=0
+
 if [[  ! -d ${APP_ENV} ]]
 then
     echo "${APP_ENV}が存在しません。" >> ${LOG}
-    exit 1
+    exit ${ERROR_EXIT_CODE}
 elif [[ ! -d ${APP_HOME} ]]
 then
     echo "${APP_HOME}が存在しません。" >> ${LOG}
-    exit 1
+    exit ${ERROR_EXIT_CODE}
 elif [[ ! -d ${APP_ID_PATH} ]]
 then
     echo "${APP_ID_PATH}が存在しません。" >> ${LOG}
-    exit 1
+    exit ${ERROR_EXIT_CODE}
 fi
 
 APP_ID=`cat ${APP_ID_PATH}`
