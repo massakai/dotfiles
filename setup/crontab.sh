ffb83d90a22488837e450fdf4af76f8ebf6275bf
Masashi SAKAI <masashi.sakai1986@gmail.com>
2017-10-27 07:17:03 +0900

    crontab.shの内容をsetup/crontab.shで実行するように修正

--- setup/crontab.sh
+++ setup/crontab.sh
@@ -1,4 +1,32 @@
 #!/bin/zsh
 
 ln -sf ${REPOSITORY_ROOT_DIR}/.crontab ~/.crontab
-crontab <(${REPOSITORY_ROOT_DIR}/crontab.sh)
+
+crontab <(cat <<EOS
+SHELL=${SHELL}
+HOME=${HOME}
+MAILTO=masashi.sakai1986@gmail.com
+PATH=${HOME}/.crontab/bin:/usr/local/bin:/usr/bin:/bin
+LOG_DIR=${HOME}/.crontab/logs
+
+EOS
+
+cat <<'EOS'
+#
+# hourly
+#
+
+# log open exchange rate
+10 * * * * oxr_getter.sh < /dev/null > ${LOG_DIR}/oxr_getter.out 2> ${LOG_DIR}/oxr_getter.err
+
+#
+# daily
+#
+
+# brew update & upgrade
+0 3 * * * brew.sh
+
+# remove old log files
+0 0 * * * find ${LOG_DIR} -name '*.out' -o -name '*.err' -mtime +30 | xargs ${XARGS_EMPTY_OPTION} rm
+EOS
+)
