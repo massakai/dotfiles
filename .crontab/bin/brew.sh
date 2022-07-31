85a4f3306f9ace2df5e471539c9c8066a7da69a6
Masashi SAKAI <masashi.sakai1986@gmail.com>
2022-07-31 19:01:26 +0900

    brewのパスを指定する

--- .crontab/bin/brew.sh
+++ .crontab/bin/brew.sh
@@ -2,16 +2,17 @@
 
 DATE=`date +\%Y-\%m-\%d`
 LOG_DIR=~/.crontab/logs/brew
+BREW=/usr/local/bin/brew
 BREW_LOG=${LOG_DIR}/$DATE.out
 BREW_ERR=${LOG_DIR}/$DATE.err
 
-if [[ $(which brew) != 0 ]]
+if [ ! -e ${BREW} ]
 then
-    echo "Homebrewがインストールされていません。" > ${BREW_ERR}
+    echo "Homebrewがインストールされていません。" >> ${BREW_ERR}
     # インストールしない環境もあるので、エラーにはしない。
     exit 0
 fi
 
 mkdir -p ${LOG_DIR}
-brew update >> ${BREW_LOG} 2>> ${BREW_ERR}
-brew upgrade >> ${BREW_LOG} 2>> ${BREW_ERR}
+${BREW} update >> ${BREW_LOG} 2>> ${BREW_ERR}
+${BREW} upgrade >> ${BREW_LOG} 2>> ${BREW_ERR}
